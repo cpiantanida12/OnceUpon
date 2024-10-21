04816a3764c4512c92786b92d140c79dc7a15aad
Hima Cheruvu <67663487+himacheruvu@users.noreply.github.com>
2024-10-20 21:05:48 -0500

    Update browse.jsx

--- Frontend/Once-Upon/app/(tabs)/browse.jsx
+++ Frontend/Once-Upon/app/(tabs)/browse.jsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import { StyleSheet, View, Text, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, View, Text, ScrollView, FlatList, Image } from 'react-native';
 
-const genres = ['Fiction', 'Non-Fiction', 'Fantasy', 'Science Fiction', 'Mystery'];
+const genres = ['Teamwork', 'Adventure', 'Friendship', 'Self-Discovery', 'Problem-Solving'];
+
+const images = [
+  require('../../assets/images/browse-page-images/zach-super-squad.jpg'),
+  require('../../assets/images/browse-page-images/luna_midnight.jpg'),
+  require('../../assets/images/browse-page-images/science-fair.jpg'),
+  require('../../assets/images/browse-page-images/river_echoes.jpg'),
+  require('../../assets/images/browse-page-images/new-homework.jpg'),
+  require('../../assets/images/browse-page-images/new-balloonrace.jpg'),
+  require('../../assets/images/browse-page-images/hidden-kingdom.jpg'),
+  require('../../assets/images/browse-page-images/new-whisperingwoods.jpg'),
+  require('../../assets/images/browse-page-images/color-thief.jpg'),
+  require('../../assets/images/browse-page-images/new-bubbles.jpg'),
+  require('../../assets/images/browse-page-images/new-timetravel.jpg'),
+  require('../../assets/images/browse-page-images/new-timejar.jpg'),
+  require('../../assets/images/browse-page-images/new-whisperingwoods.jpg'),
+  require('../../assets/images/browse-page-images/new-balloonrace.jpg'),
+  require('../../assets/images/browse-page-images/lost_feather.jpg'),
+  require('../../assets/images/browse-page-images/new-sneakers.jpg'),
+  require('../../assets/images/browse-page-images/new-stolen-stars.jpg'),
+  require('../../assets/images/browse-page-images/robot-dance.jpg'),
+  require('../../assets/images/browse-page-images/river_echoes.jpg'),
+  require('../../assets/images/browse-page-images/shadow-thief.jpg'),
+  require('../../assets/images/browse-page-images/new-missingbook.jpg'),
+  require('../../assets/images/browse-page-images/new-stolen-stars.jpg'),
+  require('../../assets/images/browse-page-images/secret-clubhouse.jpg'),
+  require('../../assets/images/browse-page-images/luna_midnight.jpg'),
+  require('../../assets/images/browse-page-images/science-fair.jpg'),
+]
 
 const BrowseScreen = () => {
   return (
     <ScrollView style={styles.container}>
-      {genres.map((genre) => (
+      {genres.map((genre, index) => (
         <View key={genre} style={styles.genreSection}>
           <Text style={styles.genreTitle}>{genre}</Text>
           <FlatList
             horizontal
-            data={[1, 2, 3, 4, 5]}
+            data={images.slice(index * 5, index * 5 + 5)}
             renderItem={({ item }) => (
               <View style={styles.bookContainer}>
                 {/* Replace this with an Image component in the future */}
-                <View style={styles.bookPlaceholder} />
+                <Image source={item} style={styles.bookImage} />
               </View>
             )}
-            keyExtractor={(item) => item.toString()}
+            keyExtractor={(item, index) => item.toString()}
             showsHorizontalScrollIndicator={false}
           />
         </View>
@@ -48,6 +76,10 @@ const styles = StyleSheet.create({
   },
   bookPlaceholder: {
     flex: 1,
+  },
+  bookImage: {
+    width: 100,
+    height: 150,
   },
 });
 
